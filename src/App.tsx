306f869b469ac198d7b1e00084d290246b4a1082
MuskokaImprovements19 <work@muskokaimprovements.com>
2026-04-30 21:56:21 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import CottageRenovations from "./pages/services/CottageRenovations";
 import WeeklyGarbage from "./pages/services/WeeklyGarbage";
 import Unsubscribe from "./pages/Unsubscribe";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
